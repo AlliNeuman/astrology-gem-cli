38773dc55e83b1ec13420b749516daa1a8796702
Allison Tzadok <allisontzadok@gmail.com>
2018-01-30 22:16:06 +0000

    added basic menu

--- lib/astrology_gem_cli/cli.rb
+++ lib/astrology_gem_cli/cli.rb
@@ -1,7 +1,27 @@
 class AstrologyGemCli::CLI
 
   def call
-    puts "Check Your Horoscope!"
+    list_horoscope_signs
+    menu
+
   end
+
+  def list_horoscope_signs
+    puts "Check Your Horoscope!"
+    puts "astrology signs"
+  end
+
+  def menu
+    puts "Enter your astrological sign or type 'exit' to exit the program."
+    while input != 'exit'
+
+    input = gets.strip.downcase
+    case input
+    when ""
+
+  end
+
+
+
 
 end
