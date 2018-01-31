b3b0badf71332253646632fdb15fae5f34305e02
Allison Tzadok <allisontzadok@gmail.com>
2018-01-31 20:27:38 +0000

    added nokorigri code to horoscope.rb, added other required open uri, pry and nokogiri to astrology_gem_cli requirement page environment

--- lib/astrology_gem_cli.rb
+++ lib/astrology_gem_cli.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "astrology_gem_cli/version"
 require_relative "astrology_gem_cli/cli"
 require_relative "astrology_gem_cli/horoscope"
